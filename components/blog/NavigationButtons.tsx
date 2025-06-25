55633877be70e7b910c23dc630a9766dd17c9112
cristoferscalante <cristoferscalante@gmail.com>
2025-06-25 12:24:28 -0500

    version 25/06

--- components/blog/NavigationButtons.tsx
+++ components/blog/NavigationButtons.tsx
@@ -64,7 +64,7 @@ export default function NavigationButtons({ direction }: NavigationButtonsProps)
           </>
         ) : (
           <>
-            Ver posts
+            Ver post
             <ChevronDown className="w-4 h-4" />
           </>
         )}
