980af87841cfb39e49fcff6b889cf0fcad7c3105
cristoferscalante <cristoferscalante@gmail.com>
2025-07-22 17:50:09 -0500

    actualizacion logo

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
     <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-custom-2/30 bg-custom-1/30 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-textMuted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-highlight focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:text-gray-900 dark:placeholder:text-gray-500",
+        "flex h-10 w-full rounded-md border border-custom-2/30 bg-custom-1/30 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-textMuted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-highlight focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-textPrimary",
         className,
       )}
       ref={ref}
